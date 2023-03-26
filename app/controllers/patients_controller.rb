7ef008bcad144b92f305b5e67d287414ffa73692
Patrick Santos <patrick.sg.santos@gmail.com>
2023-03-26 18:30:02 -0300

    patients creating working , pages to index and show already implemented

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -6,7 +6,29 @@ class PatientsController < ApplicationController
   end
 
   def create
-    raise
+    @patient = Patient.new(patient_params)
+    authorize @patient
+    @patient.save!
+
+    redirect_to patient_path(@patient)
+  end
+
+  def show
+    @patient = Patient.find(params[:id])
+    authorize @patient
+  end
+
+  def edit
+    authorize @patient
+  end
+
+  def update
+    authorize @patient
+  end
+
+  def index
+    # @patients = Patient.all
+    @patients = policy_scope(Patient)
   end
 
   private
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,3 @@
 class Patient < ApplicationRecord
-
+  enum gender: { undefined: 0, male: 1, female: 2 }
 end
