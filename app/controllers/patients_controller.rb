5aecf48be6c47d0437e63cfef51ab8e3764f8b3f
Patrick Santos <patrick.sg.santos@gmail.com>
2023-03-26 18:48:18 -0300

    patients are now able to been edited, all screen , paths and pundit policies were well created for it, only admins can do that so."

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -19,11 +19,16 @@ class PatientsController < ApplicationController
   end
 
   def edit
+    @patient = Patient.find(params[:id])
     authorize @patient
   end
 
   def update
+    @patient = Patient.find(params[:id])
     authorize @patient
+    @patient.update(patient_params)
+
+    redirect_to patient_path(@patient)
   end
 
   def index
